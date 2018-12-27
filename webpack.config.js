457abfbd24bc09ae5bd13485e519f42d271b707a
Kirill Tovpinec <kirilltovpinec@KirillTovpinets-Macbook-Air.local>
2018-12-27 11:00:57 +0300

    docs: add configuration files to the repo

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+module.exports = {
+    entry: ['./src/js/index.js', './src/styles/style.sass'],
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'styles/[name].css'
+                        }
+                    },
+                    'extract-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    watch: true,
+    devServer: {
+        contentBase: './',
+        publicPath: '/dist/'
+    },
+    plugins: [],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname,"dist")
+    },
+}
